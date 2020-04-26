f77c93b61ad4085d96bc3f7b599903afbc3f143e
Dan Njoroge <greatdaniels@gmail.com>
2020-04-26 15:51:01 +0300

    adding code to get news_sources data from our api

--- app/request.py
+++ app/request.py
@@ -1,5 +1,31 @@
 from app import app
+import urllib.request, json
+from .models import sources
+
+Sources = sources.Sources
 
 #getting api key
 
 api_key = app.config['NEWS_API_KEY']
+
+#getting the news base url
+
+base_url = app.config['NEWS_API_BASE_URL']
+
+def get_sources(category):
+    '''
+    function that gets json response to our url request
+    '''
+    get_sources_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_results(sources_results_list)
+        return sources_results
+
