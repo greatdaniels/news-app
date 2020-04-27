dc1778308d3f54f321c1387cf7945ae5d25652b2
Dan Njoroge <greatdaniels@gmail.com>
2020-04-27 14:13:06 +0300

    adding app configuration in request.py

--- app/request.py
+++ app/request.py
@@ -1,21 +1,16 @@
-from app import app
 import urllib.request, json
-from .models import sources, articles
+from .models import Sources, Articles
+from datetime import datetime
 
-Sources = sources.Sources
-Articles = articles.Articles
+api_key = None
+articles_url = None
+base_url = None
 
-#getting api key
-
-api_key = app.config['NEWS_API_KEY']
-
-#getting the news base url
-
-base_url = app.config['NEWS_API_BASE_URL']
-
-#getting articles url
-
-articles_url = app.config['NEWS_ARTICLES_API_URL']
+def configure_request(app):
+    global api_key, base_url, articles_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+    articles_url = app.config['NEWS_ARTICLES_API_URL']
 
 def get_sources(category):
     '''
