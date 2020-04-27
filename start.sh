60f6c3328f5159e174047e6112a8c164d0bb16a2
Dan Njoroge <greatdaniels@gmail.com>
2020-04-27 15:36:52 +0300

    updating files to enable easy launch of the app

---
+++ start.sh
@@ -0,0 +1,2 @@
+export NEWS_API_KEY='b7dc43c4c8604ab5837b0055a6f6a323'
+python3.6 manage.py server
--- tests/source_test.py
+++ tests/source_test.py
@@ -21,10 +21,8 @@ class SourceTest(unittest.TestCase):
         self.assertEquals(self.new_source.id, 'abc-news')
         self.assertEquals(self.new_source.name, 'ABC news')
         self.assertEquals(self.new_source.description, 'Your trusted source for breaking news')
-        self.assertEquals(self.new_source.url, 'https://bing.com')
+        self.assertEquals(self.new_source.url, "https://abcnews.go.com")
         self.assertEquals(self.new_source.category, 'general')
         self.assertEquals(self.new_source.language, 'en')
         self.assertEquals(self.new_source.country, 'us')
 
-if __name__ == '__main__':
-    unittest.main()
